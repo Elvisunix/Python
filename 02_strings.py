1fa82f8b39246796bf7c542a534a41bc6dcaa27f
Juanjo <juanjosegarcia.sl@gmail.com>
2023-05-22 23:22:24 +0200

    string

---
+++ 02_strings.py
@@ -0,0 +1,23 @@
+my_malaga  = "Viva el Malaga"
+my_andalucia = "Viva Andalucia"
+
+
+print (len(my_andalucia))
+
+print (my_andalucia + "menos que" + my_malaga)
+
+
+#Salto de linea
+my_new_line = "La afición Malagueña nunca deja nunca deja de animar\nvamos"
+print (my_new_line)
+
+#Tabulador
+my_new_line_tabulacion = "arriba arriba arriba\t basti"
+print (my_new_line_tabulacion)
+
+#Formateo
+
+name , apellido, edad = "Juanjo" , "Garcia", 36 
+
+print ("Mi nombres es {} {} y mi edad es {}".format(name,apellido,edad))
+print ("Mi nombres es %s %s y mi edad es %d" %(name,apellido,edad))
