1ac47294fec39b98060d4a678477a8f906480c25
Juanjo <juanjosegarcia.sl@gmail.com>
2023-05-22 23:41:26 +0200

    string2

--- 02_strings.py
+++ 02_strings.py
@@ -22,3 +22,37 @@ print ("Mi nombres es {} {} y mi edad es {}".format(name,apellido,edad))
 print ("Mi nombres es %s %s y mi edad es %d" %(name,apellido,edad))
 #Formatear las variables
 print (f"Mi nombre es  {name}{apellido} y mi edad es {edad}")
+
+#Desempaquetado de caracteres
+language = "python"
+a , b , c , d, e, f= language
+print (a)
+print (b)
+
+#División 
+
+language_slice = language[1:3]
+print (language_slice)
+
+language_slice = language[-2]
+print (language_slice)
+
+#Reverse_languaje
+
+reversed_language = language [::-1]
+print (reversed_language)
+
+#Funciones
+print (language.capitalize())
+print (language.upper())
+print (language.count("t"))
+print (language.isnumeric())
+print ("1".isnumeric())
+print (language.upper().isupper())
+
+
+
+
+
+
+
