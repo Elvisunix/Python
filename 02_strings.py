55f12e1bcee4ca05e782afcec10e76ae798bfdba
Juanjo <juanjosegarcia.sl@gmail.com>
2023-05-22 23:22:29 +0200

    string

--- 02_strings.py
+++ 02_strings.py
@@ -18,6 +18,7 @@ print (my_new_line_tabulacion)
 #Formateo
 
 name , apellido, edad = "Juanjo" , "Garcia", 36 
-
 print ("Mi nombres es {} {} y mi edad es {}".format(name,apellido,edad))
 print ("Mi nombres es %s %s y mi edad es %d" %(name,apellido,edad))
+#Formatear las variables
+print (f"Mi nombre es  {name}{apellido} y mi edad es {edad}")
